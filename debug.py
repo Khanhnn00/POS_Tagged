c36592c9bdcca3f18572e65ba643219d459a55a9
Khanhnn00 <18520901@gm.uit.edu.vn>
2020-12-30 14:04:58 +0700

    xD

--- debug.py
+++ debug.py
@@ -1,17 +1,30 @@
 import os
+import nltk
+# nltk.download()
+from nltk.corpus import treebank
+from nltk.probability import LaplaceProbDist
+from nltk.tag.hmm import HiddenMarkovModelTrainer
 
-words = []
-with open('data.txt', 'r') as f:
-    for x in f:
-        x = x.replace(',', '')
-        tmp = x.split()
-        for each in tmp:
-            words.append(each)
-        words.append('//')
-f.close()
-# words = set(words)
-with open('analys.csv', 'wt', encoding='utf-8' ) as f:
-    for word in words:
-        f.write(word.lower())
-        f.write('\n')
-f.close()
+taggedsents = treebank.tagged_sents()
+print(taggedsents)
+
+trainer = HiddenMarkovModelTrainer()
+tagger = trainer.train_supervised(taggedsents, estimator=LaplaceProbDist)
+sent = 'When I graduated from a less than desirable college , no employers in New York would take me seriously'
+pos = tagger.tag(sent.split())
+print(pos)
+# words = []
+# with open('data.txt', 'r') as f:
+#     for x in f:
+#         x = x.replace(',', '')
+#         tmp = x.split()
+#         for each in tmp:
+#             words.append(each)
+#         words.append('//')
+# f.close()
+# # words = set(words)
+# with open('analys.csv', 'wt', encoding='utf-8' ) as f:
+#     for word in words:
+#         f.write(word.lower())
+#         f.write('\n')
+# f.close()
