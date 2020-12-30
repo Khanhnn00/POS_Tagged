ee39bbaf62f9050688c7b5fd3d44c004dcd755b7
Khanhnn00 <18520901@gm.uit.edu.vn>
2020-12-30 11:00:59 +0700

    xD

--- debug.py
+++ debug.py
@@ -1,1 +1,17 @@
 import os
+
+words = []
+with open('data.txt', 'r') as f:
+    for x in f:
+        x = x.replace(',', '')
+        tmp = x.split()
+        for each in tmp:
+            words.append(each)
+        words.append('//')
+f.close()
+# words = set(words)
+with open('analys.csv', 'wt', encoding='utf-8' ) as f:
+    for word in words:
+        f.write(word.lower())
+        f.write('\n')
+f.close()
